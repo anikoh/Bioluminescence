f089ba429cfc3f4a36a248a89f5e354871656d1a
anikoh <anikoh@gmail.com>
2017-02-24 13:26:18 +1100

    not broken! win display working

---
+++ project1/Readme.md
@@ -0,0 +1,64 @@
+Overview
+————
+
+Bioluminescence is a game inspired by deep sea creatures.
+In terms of game play it's a cross between a memory card game and minesweeper.
+
+The board is populated with a target, several enemies, and and inert background characters ( currently 1, 3 and 15 respectively) in a randomly generated configuration.
+(This can be seen by commenting out the  switchVisibility(randomCol, randomRow); line in the populateBackground and populateElements function. Don't play the game in this mode though!)
+These elements are hidden at the start of the game.
+For each turn players can move around the board, or they can use their illuminate function to see the elements in their surroundings.
+
+I aimed to make a game that has simple rules and game play, but still rewards skill, and has several options for strategy.
+Players might choose to explore methodically, or take leaps into unknown territory and risk hitting an enemy. Players with a better memory will have to use the illuminate feature less often, and can get further in their exploration.
+
+
+
+Approach
+————
+
+The game was created in HTML CSS and vanilla Javascript.
+
+My method for planning out medium sized and bigger projects is:
+- create a flow diagram
+- based on that, make a list of all the logic elements, and all the DOM elements I need to keep track of
+- from that, decide what kind of data structures I’m going to use
+- write up a sequential list of tasks that need to happen, mark which ones are logic and which ones are DOM. These may or may not end up as separate functions.
+
+All of these things will change during the development process, but it provides a pretty good foundation.
+
+
+
+What I learned
+—————————
+
+- DOM manipulation
+- CSS styling
+- The importance of data separation and single-purpose functions. I was able to rewrite a basic method I call on constantly with ease, because it is only defined in the one place.
+- debugging & programming on the console.
+- The importance of committing frequently, after a few last minute panics when I broke all of my code.
+- Adding features and rewriting methods without breaking everything
+- developing the parameters of a project while writing the code for the project; I changed the way the game functions based on gameplay, for example, moving around the board used to be keyboard based, and players could only move one square at a time, but that turned out to be extremely frustrating.
+
+
+To do list
+——————
+
+- a function for clearing out the game elements and re-setting values, so the game can be re-started after a win without reloading
+- a display for which player’s turn it is (I had one, but I wanted to change it)
+- create images with the players overlaid with the target/enemy to use for winning, so they don’t just disappear from the board when someone wins
+- checks to stop inappropriate use:
+		- shouldn’t be able to click on the other player’s space
+		- shouldn’t be able to click on board after win
+
+
+
+
+
+
+Future Features
+——————
+
+- User input for board size, number of enemies, number of targets
+- animating the illuminate function: perhaps radiating out from the player, or fading or flickering, or some combination
+- deep sea background artwork/ deep sea fish images for players (the ones I had didn’t work at such a small size. The current fishes are tropical, not deep sea, terribly inaccurate.
--- project1/game.js
+++ project1/game.js
@@ -1,24 +1,20 @@
-//visibility
-//     document.querySelector("[data-row='0'][data-col='10']").style.visiblity = "visible"
-//     document.querySelector("[data-row='0'][data-col='10']").innerHTML = '<img class="is-visible" src="img/jellyfish.jpg">'
-
 
 
 
 // Global variables for game elements. Players, target, enemies, background characters
 
   var player1 = {
+    name: "Player 1",
     col: 0,
     row: 0,
-    lights: 5,
-    image: '<img src="img/fish3.jpg">'
+    image: "img/bluefish.png"
   }
 
   var player2 = {
+    name: "Player 2",
     col: 0,
     row: 0,
-    lights: 5,
-    image: '<img src="img/fish1red.jpg">'
+    image: "img/stripeyfish.png"
   }
 
 var elements =[
@@ -26,25 +22,25 @@ var elements =[
   target1 = {
     col: 0,
     row: 0,
-    image: '<img src="img/fish2.jpg">'
+    image: "img/starfish.jpg"
   },
 
   enemy1 = {
     col: 0,
     row: 0,
-    image: '<img src="img/octopus.jpg">'
+    image: "img/octopus.png"
   },
 
   enemy2 = {
     col: 0,
     row: 0,
-    image: '<img src="img/squid.jpg">'
+    image: "img/octopus.png"
   },
 
   enemy3 = {
     col: 0,
     row: 0,
-    image: '<img src="img/squid2.jpg">'
+    image: "img/octopus.png"
   },
 ]
 
@@ -54,19 +50,19 @@ backgroundChars = [
   plant1 = {
     col: 0,
     row: 0,
-    image: '<img src="img/plant.jpg">'
+    image: "img/plant.jpg"
   },
 
   plant2 = {
     col: 0,
     row: 0,
-    image: '<img src="img/jellyfish.jpg">'
+    image: "img/jellyfish.jpg"
   },
 
   plant3 = {
     col: 0,
     row: 0,
-    image: '<img src="img/jellyfish2.jpg">'
+    image: "img/jellyfish2.jpg"
   }
 ];
 
@@ -75,6 +71,8 @@ var boardRow=9;
 var backgroundCounter= 15;
 
 var currentPlayer;
+var otherPlayer;
+var boardExists = false;
 
 
 
@@ -86,10 +84,19 @@ function getRandomInt(min, max) {
 // places a character on a card at col row, updates the co-ordinates of character to reflect this
 
 var placeOnCard = function(character, col, row){
+//  var oldCard = document.querySelector("[data-row='" +(character.row).toString() + "'][data-col='" +(character.col).toString() + "']");
+  var newCard = document.querySelector("[data-row='" +row.toString() + "'][data-col='" +col.toString() + "']");
 
-  var card = document.querySelector("[data-row='" +row.toString() + "'][data-col='" +col.toString() + "']");
-
-  card.innerHTML = character.image;
+  // if(oldCard){
+  //   newImage = oldCard.children[0];
+  //   newCard.appendChild(newImage);
+  // }
+  // else{
+    var newImage = document.createElement('img');
+    newImage.src = character.image;
+    newCard.appendChild(newImage);
+//  }
+ //console.log(newCard);
 
   character.col = col;
   character.row = row;
@@ -99,11 +106,87 @@ var placeOnCard = function(character, col, row){
 // removes the character from the card that matches it's coordinates. It leaves the co-ordinate
 // attributes of the character unchanged!!!!!!
 var removeFromCard = function(character){
-
   var card = document.querySelector("[data-row='" +(character.row).toString() + "'][data-col='" +(character.col).toString() + "']");
-  card.innerHTML = "";
+  //card.innerHTML = "";
+  card.children[0].remove();
 }
 
+// toggles the visibility of the image on the card at the given co-ordinates
+// doesn't seem to need the character?!!!!!!
+var switchVisibility = function(col, row){
+  var card = document.querySelector("[data-row='" +row.toString() + "'][data-col='" +col.toString() + "']");
+
+  if(card.children[0]){
+    card.children[0].classList.toggle("hidden");
+  }
+}
+
+
+// changes the visibility of the area defined by the input values, excluding
+// the players
+
+var switchVisibilityArea = function(minCol, maxCol, minRow, maxRow){
+  for(var i=minCol; i<maxCol; i++){
+    for(var j=minRow; j<maxRow; j++){
+      if(!(i=== player1.col && j === player1.row)&&!(i=== player2.col && j === player2.row)){  //exclude the players from the visibility toggle
+        switchVisibility(i, j);
+        var card = document.querySelector("[data-row='" +j + "'][data-col='" +i + "']");
+        card.classList.toggle("illuminated");
+      }
+    }
+  }
+
+}
+
+//
+var illuminate= function(character){
+  var range = 2;
+  var minCol;
+  var maxCol;
+  var minRow;
+  var maxRow;
+
+  if(character.col-range < 0){
+    minCol= 0;
+  }
+  else{
+    minCol = character.col-range;
+  }
+
+  if(character.col+range > boardCol){
+    maxCol = boardCol;
+  }
+  else{
+    maxCol = character.col+range;
+  }
+
+  if(character.row-range < 0){
+    minRow= 0;
+  }
+  else{
+    minRow = character.row-range;
+  }
+
+  if(character.row+range > boardRow){
+    maxRow = boardRow;
+  }
+  else{
+    maxRow = character.row+range;
+  }
+
+// illuminate the area, wait, then switch back to hidden
+  switchVisibilityArea(minCol, maxCol, minRow, maxRow);
+  setTimeout(function(){switchVisibilityArea(minCol, maxCol, minRow, maxRow);}, 1000);
+
+
+}
+
+
+var displayWinner = function(character){
+  document.querySelector(".winner").childNodes[1].textContent = character.name + " wins!";
+  document.querySelector(".winner").childNodes[2].src = character.image;
+  document.querySelector(".winner").classList.toggle("collapse");
+}
 
 //dynamically creates the cards that make up the board, based on width and height input
 
@@ -122,152 +205,154 @@ var generateBoard = function(column, row){
 
 // populates the board with inert background characters
 var populateBackground = function(counter){
-  for (var i=0; i< counter; i++){
+  var i =0;
+  while(i< counter){
     var randomCol = getRandomInt(0, boardCol-1);
     var randomRow = getRandomInt(0, boardRow-1);
     var randomChar = getRandomInt(0, backgroundChars.length -1);
-    placeOnCard(backgroundChars[randomChar], randomCol, randomRow);
+    var card = document.querySelector("[data-row='" +randomRow.toString() + "'][data-col='" +randomCol.toString() + "']");
+    if(!card.children[0]){
+      placeOnCard(backgroundChars[randomChar], randomCol, randomRow);
+      switchVisibility(randomCol, randomRow);
+      i++;
+    }
   }
 }
 
-// places target and enemies on the board, checking that they're not overwriting an already existing character
-// later make sure none of the elements are overwritten!!!
+// places target and enemies on the board, checking that they're not overwriting any already existing characters
 
 var populateElements = function(){
-  for(var i=0; i< elements.length; i++){
-  var randomCol = getRandomInt(0, boardCol-1);
-  var randomRow = getRandomInt(0, boardRow-1);
+  var i=0;
+  while(i< elements.length){
+    var randomCol = getRandomInt(0, boardCol-1);
+    var randomRow = getRandomInt(0, boardRow-1);
 
-  placeOnCard(elements[i], randomCol, randomRow);
+    var card = document.querySelector("[data-row='" +randomRow.toString() + "'][data-col='" +randomCol.toString() + "']");
+    if(!card.children[0]){
+      placeOnCard(elements[i], randomCol, randomRow);
+      switchVisibility(randomCol, randomRow);
+      i++;
+    }
   }
 }
 
-var illuminateBoard = function(){
-
-}
 
 
 // initialise the game
 
-document.body.addEventListener('keydown', function(event){
-  var input = event.which;
+    //document.querySelector('.start-game').addEventListener('click', function(event){
+    //var input = event.which;
 
-  //on return
-  if(input === 13){
+    document.body.addEventListener('keydown', function(event){
+      var input = event.which;
+
+      //on return
+    if(input === 13){
+    document.querySelector(".instructions").classList.toggle("collapse");
+
     generateBoard(boardCol, boardRow);
-    populateBackground(backgroundCounter);
-    populateElements();
 
     placeOnCard(player1, 0, boardRow-1);
     placeOnCard(player2, boardCol-1, 0);
 
-    player1.image ='<img class="is-visible" src="img/fish3.jpg">';
+    //give the player images a unique class
+    var card1 = document.querySelector("[data-row='" +(boardRow-1).toString() + "'][data-col='" +(0).toString() + "']");
+    card1.children[0].className = "player";
+    var card2 = document.querySelector("[data-row='" +(0).toString() + "'][data-col='" +(boardCol-1).toString() + "']");
+    card2.children[0].className = "player";
 
-    illuminateBoard();
+
+    populateElements();
+    populateBackground(backgroundCounter);
+
+    //player1.image ='<img class="is-visible" src="img/fish3.jpg">';
+
+    //illuminateBoard();
 
     // player1 starts
     currentPlayer = player1;
-    document.querySelector('.player1').classList.toggle("current", player1 === currentPlayer );
-
+    otherPlayer = player2;
+    //document.querySelector('.player1').classList.toggle("current", player1 === currentPlayer );
+    boardExists = true;
 
   }
 });
 
 
-// the keyboard input version
-// each turn of the game
-//document.body.addEventListener('keydown', function(event){
-//
-//   var input = event.which;
-//
-//   if(input === 32 || (input === 37 && currentPlayer.col > 0) || (input === 38 && currentPlayer.row > 0) || (input === 39 && currentPlayer.col+1 < boardCol)|| (input === 40 && currentPlayer.row+1 < boardRow)){
-//
-//     // space bar 32
-//     // left arrow	37 col-1
-//     // up arrow	38 row-1
-//     // right arrow	39 col+1
-//     // down arrow	40 row+1
-//     var newRow = currentPlayer.row;
-//     var newCol = currentPlayer.col;
-//
-//
-//     if(input === 32){
-//       illuminate(currentPlayer);
-//     }
-//     else if(input === 37){
-//       newCol--;
-//     }
-//     else if(input === 38){
-//       newRow--;
-//     }
-//     else if(input === 39){
-//       newCol++;
-//     }
-//     else if(input === 40){
-//       newRow++;
-//     }
-//
-//
-
-
 document.querySelector('.board').addEventListener('click', function(event){
 
-  if(event.target.className === "card"){
-
-      //check for win and lose conditions
-    // win
-    // if(newCol === target1.col && newRow === target1.row){
-    //  console.log(currentPlayer + "wins!");
-    // }
-    // else if(newCol === enemy1.col && newRow === enemy1.row){
-    //   console.log(currentPlayer + "loses!");
+//  if(boardExists){
     //
-    // }
-    if(event.target.dataset.col === currentPlayer.col && event.target.dataset.row === currentPlayer.row){
+    //select the card that's being clicked, not the image on it
+    var targetCard;
+    if(event.target.nodeName == "IMG"){
+      targetCard = event.target.parentNode;
+    }
+    else{
+      targetCard =event.target;
+    }
+
+    var newCol = parseInt(targetCard.dataset.col);
+    var newRow = parseInt(targetCard.dataset.row);
+
+
+    // if clicks on self, illuminate surrounding area
+    if(newCol === currentPlayer.col && newRow === currentPlayer.row){
       illuminate(currentPlayer);
     }
+    //check for target
+    else if(newCol === elements[0].col && newRow === elements[0].row){
+      removeFromCard(currentPlayer);
+      switchVisibility(newCol, newRow);
+      displayWinner(currentPlayer);
+    }
+    //check for enemies
+    else if((newCol === elements[1].col && newRow === elements[1].row)||(newCol === elements[2].col && newRow === elements[2].row)||(newCol === elements[3].col && newRow === elements[3].row)){
+      removeFromCard(currentPlayer);
+      switchVisibility(newCol, newRow);
+      displayWinner(otherPlayer);
+    }
+
+    // write a function to check enemies, returns the index of the enemy in the array
+    // else if(newCol === elements[i].col && newRow === elements[i].row){
+    //   removeFromCard(currentPlayer);
+    //   switchVisibility(elements[i].col, elements[i].row);
+    //   displayWinner(otherPlayer);
+    // }
+
+    //check for background characters, if present remove them
+    else if(targetCard.children[0]){
+      targetCard.children[0].remove();
+      removeFromCard(currentPlayer);
+      placeOnCard(currentPlayer, newCol, newRow);
+    }
+    //move player
     else {
       removeFromCard(currentPlayer);
-      placeOnCard(currentPlayer, event.target.dataset.col, event.target.dataset.row);
+      placeOnCard(currentPlayer, newCol, newRow);
     }
 
 
     //toggle the Players
     if(currentPlayer === player1){
       currentPlayer = player2;
+      otherPlayer = player1;
     }
     else{
       currentPlayer =player1;
+      otherPlayer = player2;
     }
 
     //highlight current player
-    document.querySelector('.player1').classList.toggle("current", player1 === currentPlayer );
-    document.querySelector('.player2').classList.toggle("current", player2 === currentPlayer );
-  }
+
+    // border around player
+    // var newCol = currentPlayer.col;
+    // var newRow = currentPlayer.row;
+    // var card = document.querySelector("[data-row='" + newRow + "'][data-col='" + newCol + "']");
+    // card.classList.toggle("current-player");
+
+    // highlight side box
+    //document.querySelector('.player1').classList.toggle("current", player1 === currentPlayer );
+    //document.querySelector('.player2').classList.toggle("current", player2 === currentPlayer );
+//  }
 });
-
-
-
-
-
-var insertCharacter = function(column, row){
-  var randomCol = getRandomInt(0, column-1);
-  var randomRow = getRandomInt(0, row-1);
-
-  enemy1.col = randomCol;
-  enemy1.row = randomRow;
-
-//test =document.querySelector("[data-row='3'][data-col='1']")
-
-  var enemySquare= document.querySelector("[data-row='" +randomRow.toString() + "'][data-col='" +randomCol.toString() + "']");
-
-  enemySquare.innerHTML = enemy1.image;
-
-}
-
-
-
-// creates num x enemy objects, returns them as a string
-var createEnemies = function(num){
-
-}
